76ef456640780609f0645ba39a619c610e48dd16
Raghav Dhingra <raghav.dhingra15@gmail.com>
2021-05-11 21:46:08 +0530

    social updated

--- src/actions/defaultApps.js
+++ src/actions/defaultApps.js
@@ -11,6 +11,14 @@ import Settings from '../components/applications/settings/settings';
 import PROFILE_IMAGE from '../assets/icons/profile.svg';
 import PROJECT_IMAGE from '../assets/icons/project.svg';
 import ARCHIVE_IMAGE from '../assets/icons/archive.svg';
+import GITHUB_IMAGE from '../assets/icons/octocat.svg';
+import FACEBOOK_IMAGE from '../assets/icons/facebook.svg';
+import TWITTER_IMAGE from '../assets/icons/twitter.svg';
+import INSTAGRAM_IMAGE from '../assets/icons/instagram.svg';
+import LINKEDIN_IMAGE from '../assets/icons/linkedin.svg';
+import CODEPEN_IMAGE from '../assets/icons/codepen.svg';
+import GMAIL_IMAGE from '../assets/icons/gmail.svg';
+import MEDIUM_IMAGE from '../assets/icons/medium.svg';
 
 // const nullFunction = (supplement) => null;
 const width1 = '40px';
@@ -83,6 +91,70 @@ export const applications = {
       bigWidth: width3,
       image: PROJECT_IMAGE,
       link: 'https://portfolio.raghavdhingra.com/projects',
+    },
+    {
+      name: 'GitHub',
+      key: 'github',
+      width: width1,
+      bigWidth: width3,
+      image: GITHUB_IMAGE,
+      link: 'https://github.com/raghavdhingra',
+    },
+    {
+      name: 'Facebook',
+      key: 'facebook',
+      width: width1,
+      bigWidth: width3,
+      image: FACEBOOK_IMAGE,
+      link: 'https://www.facebook.com/raghav.dhingra15',
+    },
+    {
+      name: 'Twitter',
+      key: 'twitter',
+      width: width1,
+      bigWidth: width3,
+      image: TWITTER_IMAGE,
+      link: 'https://twitter.com/raghavdhingra15',
+    },
+    {
+      name: 'Instagram',
+      key: 'instagram',
+      width: width1,
+      bigWidth: width3,
+      image: INSTAGRAM_IMAGE,
+      link: 'https://www.instagram.com/raghav.dhingra15/',
+    },
+    {
+      name: 'Linkedin',
+      key: 'linkedin',
+      width: width1,
+      bigWidth: width3,
+      image: LINKEDIN_IMAGE,
+      link: 'https://www.linkedin.com/in/raghav-dhingra/',
+    },
+    {
+      name: 'Codepen',
+      key: 'codepen',
+      width: width1,
+      bigWidth: width3,
+      image: CODEPEN_IMAGE,
+      link: 'https://codepen.io/raghav-dhingra',
+    },
+    {
+      name: 'GMail',
+      key: 'gmail',
+      width: width1,
+      bigWidth: width3,
+      image: GMAIL_IMAGE,
+      link: 'mailto:admin@raghavdhingra.com',
+    },
+    {
+      name: 'Medium',
+      key: 'medium',
+      width: width1,
+      bigWidth: width3,
+      image: MEDIUM_IMAGE,
+      link: 'https://medium.com/@raghav.dhingra15',
     },
   ],
   defaultApps: [

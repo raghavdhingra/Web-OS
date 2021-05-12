b2560c81a467ae43dfbeb4c5ed3f6ade7dc19dc1
Raghav Dhingra <raghav.dhingra15@gmail.com>
2021-05-12 20:02:02 +0530

    updated sw configurations

---
+++ public/sw.js
@@ -0,0 +1,34 @@
+const CACHE_NAME = 'pwa-task-manager';
+var urlsToCache = ['./'];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      // Cache hit - return response
+      if (response) return response;
+      return fetch(event.request);
+    })
+  );
+});
+self.addEventListener('activate', (event) => {
+  var cacheWhitelist = ['pwa-task-manager'];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
